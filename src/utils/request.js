70dac948ac220a82ca1c0fd5d1fa8dfe97e922c5
P1kaj1uu <891523233@qq.com>
2022-06-02 12:33:55 +0800

    优化进度条加载、路由懒加载、打包配置等

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,10 @@
 import theAxios from 'axios'
 // 导入封装的token方法
 import { getToken } from './token.js'
+// 导入进度条效果
+import NProgress from 'nprogress'
+// 导入进度条样式
+import 'nprogress/nprogress.css'
 
 const axios = theAxios.create({
   baseURL: 'http://127.0.0.1:8888/api/private/v1',
@@ -12,6 +16,7 @@ const axios = theAxios.create({
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
   // 在发送请求之前做些什么
+  NProgress.start()
   // 为请求头添加token验证Authorization字段
   config.headers.Authorization = getToken()
   return config
@@ -23,6 +28,7 @@ axios.interceptors.request.use(function (config) {
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
   // 对响应数据做点什么
+  NProgress.done()
   return response
 }, function (error) {
   // 对响应错误做点什么
